441fdadcefec05d893cace94e794cdc263c32b10
smallchill <smallchill@163.com>
2020-06-08 16:55:37 +0800

    :tada: 新增加密解密工具

--- src/util/crypto.js
+++ src/util/crypto.js
@@ -1,19 +1,19 @@
 import CryptoJS from 'crypto-js'
 
 export default class crypto {
-  //使用AesUtil.genAesKey()生成,需和后端配置保持一致
+  // 使用AesUtil.genAesKey()生成,需和后端配置保持一致
   static aesKey = "O2BEeIv399qHQNhD6aGW8R8DEj4bqHXm";
 
-  //使用DesUtil.genDesKey()生成,需和后端配置保持一致
+  // 使用DesUtil.genDesKey()生成,需和后端配置保持一致
   static desKey = "jMVCBsFGDQr1USHo";
 
   /**
    * aes 加密方法，同java：AesUtil.encryptToBase64(text, aesKey);
    */
   static encryptAES(data, key) {
-    let dataBytes = CryptoJS.enc.Utf8.parse(data);
-    let keyBytes = CryptoJS.enc.Utf8.parse(key);
-    let encrypted = CryptoJS.AES.encrypt(dataBytes, keyBytes, {
+    const dataBytes = CryptoJS.enc.Utf8.parse(data);
+    const keyBytes = CryptoJS.enc.Utf8.parse(key);
+    const encrypted = CryptoJS.AES.encrypt(dataBytes, keyBytes, {
       iv: keyBytes,
       mode: CryptoJS.mode.CBC,
       padding: CryptoJS.pad.Pkcs7
@@ -25,8 +25,8 @@ export default class crypto {
    * aes 解密方法，同java：AesUtil.decryptFormBase64ToString(encrypt, aesKey);
    */
   static decryptAES(data, key) {
-    let keyBytes = CryptoJS.enc.Utf8.parse(key);
-    let decrypted = CryptoJS.AES.decrypt(data, keyBytes, {
+    const keyBytes = CryptoJS.enc.Utf8.parse(key);
+    const decrypted = CryptoJS.AES.decrypt(data, keyBytes, {
       iv: keyBytes,
       mode: CryptoJS.mode.CBC,
       padding: CryptoJS.pad.Pkcs7
@@ -38,8 +38,8 @@ export default class crypto {
    * des 加密方法，同java：DesUtil.encryptToBase64(text, desKey)
    */
   static encryptDES(data, key) {
-    let keyHex = CryptoJS.enc.Utf8.parse(key);
-    let encrypted = CryptoJS.DES.encrypt(data, keyHex, {
+    const keyHex = CryptoJS.enc.Utf8.parse(key);
+    const encrypted = CryptoJS.DES.encrypt(data, keyHex, {
       mode: CryptoJS.mode.ECB,
       padding: CryptoJS.pad.Pkcs7
     });
@@ -50,8 +50,8 @@ export default class crypto {
    * des 解密方法，同java：DesUtil.decryptFormBase64(encryptBase64, desKey);
    */
   static decryptDES(data, key) {
-    let keyHex = CryptoJS.enc.Utf8.parse(key);
-    let decrypted = CryptoJS.DES.decrypt({
+    const keyHex = CryptoJS.enc.Utf8.parse(key);
+    const decrypted = CryptoJS.DES.decrypt({
       ciphertext: CryptoJS.enc.Base64.parse(data)
     }, keyHex, {
       mode: CryptoJS.mode.ECB,

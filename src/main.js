c1ac619afaaedbf03bff4c691a227bfa6198fac4
smallchill <smallchill@163.com>
2021-03-02 12:12:41 +0800

    :tada: 升级avue2.8.2，回归search-slot的row属性，推荐大家在缓冲期改为最新版API，后续这个属性仍可能删除，具体请看：https://sns.bladex.vip/q-4370.html

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ Vue.use(Element, {
 Vue.use(window.AVUE, {
   size: 'small',
   tableSize: 'small',
-  calcHeight: 60,
+  calcHeight: 65,
   i18n: (key, value) => i18n.t(key, value)
 });
 // 注册全局容器

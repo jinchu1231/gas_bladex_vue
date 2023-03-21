ae6f71a79b45a3e7b68deb8659a106ca58e15c74
smallchill <smallchill@163.com>
2023-03-22 00:58:00 +0800

    :zap: 导出api增加参数

--- src/api/common.js
+++ src/api/common.js
@@ -3,10 +3,13 @@ import request from "@/router/axios";
 /**
  * 文件流返回
  * @param url 接口地址
+ * @param params 接口参数
  */
-export const exportBlob = (url) => {
+export const exportBlob = (url, params) => {
+  debugger
   return request({
     url: url,
+    params: params,
     method: 'get',
     responseType: 'blob'
   })
